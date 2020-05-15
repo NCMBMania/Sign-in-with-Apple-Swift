1b2f2ea85832a96ee5d27eca6cae4bea9bff8198
Atsushi <atsushi@moongift.jp>
2020-05-15 15:30:07 +0900

    Initial Commit

---
+++ ncmb.signinwithapple/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ncmb.signinwithapple
+//
+//  Created by Atsushi on 2020/05/15.
+//  Copyright © 2020 Atsushi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
